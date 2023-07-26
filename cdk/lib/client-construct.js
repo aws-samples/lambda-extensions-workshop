3cf5653828cef57c327e342b03a8df6ca0eddd97
Thiago Padua <tgpadua@gmail.com>
2023-07-25 19:26:30 -0700

    fix: changed lambda function cdk resource

--- cdk/lib/client-construct.js
+++ cdk/lib/client-construct.js
@@ -1,42 +1,46 @@
 const cdk = require('aws-cdk-lib/core');
 const {Construct} = require('constructs');
-const {NodejsFunction} = require('aws-cdk-lib/aws-lambda-nodejs');
+const lambda = require("aws-cdk-lib/aws-lambda");
 
 class ClientConstruct extends Construct {
     constructor(scope, id, props) {
         super(scope, id);
 
-        const function1 = new NodejsFunction(this, 'function1', {
+        const function1 = new lambda.Function(this, 'function1', {
             functionName: 'lew-function1',
-            entry: 'lambda/function.js',
-            handler: 'handler',
+            code:  lambda.Code.fromAsset('lambda/client'),
+            runtime: lambda.Runtime.NODEJS_18_X,
+            architecture:  lambda.Architecture.ARM_64,
+            handler: 'function.handler',
             timeout: cdk.Duration.seconds(10),
             environment: {
-                'THROW_ERRORS': 'false'
+                THROW_ERRORS: 'false'
             }
         });
 
-        const function2 = new NodejsFunction(this, 'function2', {
+        const function2 = new lambda.Function(this, 'function2', {
             functionName: 'lew-function2',
-            entry: 'lambda/function.js',
-            handler: 'handler',
+            code:  lambda.Code.fromAsset('lambda/client'),
+            runtime: lambda.Runtime.NODEJS_18_X,
+            architecture:  lambda.Architecture.ARM_64,
+            handler: 'function.handler',
             timeout: cdk.Duration.seconds(10),
             environment: {
-                'THROW_ERRORS': 'false'
+                THROW_ERRORS: 'false'
             }
         });
 
-        const function3 = new NodejsFunction(this, 'function3', {
+        const function3 = new lambda.Function(this, 'function3', {
             functionName: 'lew-function3',
-            entry: 'lambda/function.js',
-            handler: 'handler',
+            code:  lambda.Code.fromAsset('lambda/client'),
+            runtime: lambda.Runtime.NODEJS_18_X,
+            architecture:  lambda.Architecture.ARM_64,
+            handler: 'function.handler',
             timeout: cdk.Duration.seconds(10),
             environment: {
-                'THROW_ERRORS': 'true'
+                THROW_ERRORS: 'true'
             }
         });
-
-
     }
 }
 
