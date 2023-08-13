a56f8eae33188a30ee5de35840a8207262c62d14
Young <youngjeong46@gmail.com>
2023-08-13 16:47:03 -0500

    revised scripts

--- resize-cloud9.sh
+++ resize-cloud9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Specify the desired volume size in GiB as a command line argument. If not specified, default to 20 GiB.
-SIZE=${1:-20}
+# Specify the desired volume size in GiB as a command line argument. If not specified, default to 30 GiB.
+SIZE=${1:-30}
 
 # Get the ID of the environment host Amazon EC2 instance.
 INSTANCEID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
